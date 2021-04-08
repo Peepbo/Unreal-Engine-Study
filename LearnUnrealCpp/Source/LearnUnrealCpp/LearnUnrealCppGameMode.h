8d24bf4d6495faf68dc5da835a13d363ae140fd7
Peepbo <tngus10778@gmail.com>
2021-04-08 23:01:38 +0900

    Third Person Game(Tutorial)

---
+++ LearnUnrealCpp/Source/LearnUnrealCpp/LearnUnrealCppGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LearnUnrealCppGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALearnUnrealCppGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALearnUnrealCppGameMode();
+};
+
+
+
