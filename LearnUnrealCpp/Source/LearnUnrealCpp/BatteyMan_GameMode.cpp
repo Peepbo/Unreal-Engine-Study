cda7b2fc15787f7d72a453c1c27d535d05eaf4f3
Peepbo <tngus10778@gmail.com>
2021-04-09 23:31:20 +0900

    BatteryMan Game end

--- LearnUnrealCpp/Source/LearnUnrealCpp/BatteyMan_GameMode.cpp
+++ LearnUnrealCpp/Source/LearnUnrealCpp/BatteyMan_GameMode.cpp
@@ -2,4 +2,35 @@
 
 
 #include "BatteyMan_GameMode.h"
+#include "GameFramework/Actor.h"
 
+ABatteyMan_GameMode::ABatteyMan_GameMode()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void ABatteyMan_GameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FTimerHandle UnusedHandle;
+	GetWorldTimerManager().SetTimer(
+		UnusedHandle, this, &ABatteyMan_GameMode::SpawnPlayerRecharge,
+		FMath::RandRange(2, 5), true);
+}
+
+void ABatteyMan_GameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ABatteyMan_GameMode::SpawnPlayerRecharge()
+{
+	float RandX = FMath::RandRange(Spawn_X_Min, Spawn_X_Max);
+	float RandY = FMath::RandRange(Spawn_Y_Min, Spawn_Y_Max);
+
+	FVector SpawnPosition = FVector(RandX, RandY, Spawn_Z);
+	FRotator SpawnRotation = FRotator(0.0f, 0.0f, 0.0f);
+
+	GetWorld()->SpawnActor(PlayerRecharge, &SpawnPosition, &SpawnRotation);
+}
--- LearnUnrealCpp/Source/LearnUnrealCpp/BatteyMan_GameMode.h
+++ LearnUnrealCpp/Source/LearnUnrealCpp/BatteyMan_GameMode.h
@@ -13,5 +13,28 @@ UCLASS()
 class LEARNUNREALCPP_API ABatteyMan_GameMode : public AGameMode
 {
 	GENERATED_BODY()
-	
+
+	ABatteyMan_GameMode();
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<APawn> PlayerRecharge;
+
+	float Spawn_Z = 500.0f;
+
+	UPROPERTY(EditAnywhere)
+		float Spawn_X_Min;
+
+	UPROPERTY(EditAnywhere)
+		float Spawn_X_Max;
+
+	UPROPERTY(EditAnywhere)
+		float Spawn_Y_Min;
+
+	UPROPERTY(EditAnywhere)
+		float Spawn_Y_Max;
+
+	void SpawnPlayerRecharge();
 };
