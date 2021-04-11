b13015e95abd44d3599871a0c75eb2078221ca06
Peepbo <tngus10778@gmail.com>
2021-04-11 16:25:36 +0900

    reset project

--- UnrealCpp/Source/UnrealCpp/UnrealCpp.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "UnrealCpp.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealCpp, "UnrealCpp" );
-
-DEFINE_LOG_CATEGORY(MyLogCategory);
