9724f79d16a6caa21df73472d2b0dd71379e58d4
Peepbo <tngus10778@gmail.com>
2021-04-15 12:31:06 +0900

    make Foodie, set Position!

--- Game0/Source/Game0/PacmanPawn.cpp
+++ Game0/Source/Game0/PacmanPawn.cpp
@@ -3,6 +3,8 @@
 
 #include "PacmanPawn.h"
 
+#include "Foodie.h"
+
 // Sets default values
 APacmanPawn::APacmanPawn()
 {
@@ -59,5 +61,9 @@ void APacmanPawn::SetDirection(const FVector Direction)
 
 void APacmanPawn::OnOverlapBegin(AActor* PlayerActor, AActor* OtherActor)
 {
+	if (OtherActor->ActorHasTag("Foodie.Regular"))
+	{
+		Cast<AFoodie>(OtherActor)->Consume();
+	}
 }
 
